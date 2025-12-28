0d4a8e3f932e390123b56441bd6b19733949673a
UCHIHABOTS <txkuze@gmail.com>
2025-12-29 00:21:08 +0530

    Create fed_watcher.py

---
+++ UCHIHA/plugins/watcher/fed_watcher.py
@@ -0,0 +1,24 @@
+from pyrogram import filters
+from pyrogram.errors import ChatAdminRequired
+
+from UCHIHA import app
+from UCHIHA.utils.feds_funcs import get_chat_fed, is_fedbanned
+
+
+@app.on_message(filters.group & filters.incoming)
+async def fed_auto_ban(_, message):
+    if not message.from_user:
+        return
+
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+
+    fed_id = await get_chat_fed(chat_id)
+    if not fed_id:
+        return
+
+    if await is_fedbanned(fed_id, user_id):
+        try:
+            await message.chat.ban_member(user_id)
+        except ChatAdminRequired:
+            pass
