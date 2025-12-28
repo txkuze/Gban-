d6301138137a02573cd6b92bd61faad77739a049
UCHIHABOTS <txkuze@gmail.com>
2025-12-29 00:19:32 +0530

    Create feds_db.py

---
+++ UCHIHA/utils/feds_db.py
@@ -0,0 +1,10 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_DB_URI
+
+mongo = AsyncIOMotorClient(MONGO_DB_URI)
+db = mongo.UCHIHA
+
+feds = db.feds
+fed_chats = db.fed_chats
+fed_bans = db.fed_bans
+fed_admins = db.fed_admins
