1be91a80fa860ec5069e68bce4fc048de72e9f80
UCHIHABOTS <txkuze@gmail.com>
2025-12-29 20:15:29 +0530

    Update call.py

--- UCHIHA/core/call.py
+++ UCHIHA/core/call.py
@@ -36,7 +36,7 @@ from UCHIHA.utils.formatters import check_duration, seconds_to_min, speed_conver
 from UCHIHA.utils.inline.play import stream_markup, telegram_markup
 from UCHIHA.utils.stream.autoclear import auto_clean
 from UCHIHA.utils.thumbnails import get_thumb
-from UCHIHA.strings import get_string
+from strings import get_string
 
 autoend = {}
 counter = {}
