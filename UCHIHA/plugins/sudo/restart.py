da3fa1e21517eb33eabface8b5023b790bc3287d
UCHIHABOTS <txkuze@gmail.com>
2025-12-29 16:19:09 +0530

    Update restart.py

--- UCHIHA/plugins/sudo/restart.py
+++ UCHIHA/plugins/sudo/restart.py
@@ -24,7 +24,7 @@ from UCHIHA.utils.database import (
     remove_active_video_chat,
 )
 from UCHIHA.utils.decorators.language import language
-from UCHIHA.utils.pastebin import RAUSHANBin
+from UCHIHA.utils.pastebin import PARTHBin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -85,7 +85,7 @@ async def update_(client, message, _):
     _update_response_ = "<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<b><u>ᴜᴩᴅᴀᴛᴇs:</u></b>\n\n"
     _final_updates_ = _update_response_ + updates
     if len(_final_updates_) > 4096:
-        url = await RAUSHANBin(updates)
+        url = await PARTHBin(updates)
         nrs = await response.edit(
             f"<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<u><b>ᴜᴩᴅᴀᴛᴇs :</b></u>\n\n<a href={url}>ᴄʜᴇᴄᴋ ᴜᴩᴅᴀᴛᴇs</a>"
         )
